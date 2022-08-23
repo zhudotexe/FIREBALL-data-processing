e8c1fbb5261bfa619e7c3fe4d8d5a8334fe84e32
Andrew Zhu <me@andrew-zhu.com>
2022-08-23 12:38:02 -0400

    feat: avrae event types

--- explorer/src/avrae/events.ts
+++ explorer/src/avrae/events.ts
@@ -2,6 +2,8 @@
  * Models implemented from https://github.com/avrae/avrae/blob/master/cogs5e/initiative/upenn_nlp.py
  */
 
+import type {BigNumber} from 'bignumber.js';
+
 export interface Event {
     combat_id: string;
     event_type: string;
@@ -9,20 +11,20 @@ export interface Event {
 }
 
 export interface MessageEvent extends Event {
-    event_type: 'message';
-    message_id: number;
+    event_type: 'message' | 'command';  // interface extension weirdness
+    message_id: BigNumber;
     author_id: string;
     author_name: string;
     created_at: number;
     content: string;
     embeds: any[];
     components: any[];
-    referenced_message_id?: number;
+    referenced_message_id?: BigNumber;
 }
 
 export interface AliasResolutionEvent extends Event {
     event_type: 'alias_resolution';
-    message_id: number;
+    message_id: BigNumber;
     alias_name: string;
     alias_body: string;
     content_before: string;
@@ -32,13 +34,13 @@ export interface AliasResolutionEvent extends Event {
 
 export interface SnippetResolutionEvent extends Event {
     event_type: 'snippet_resolution';
-    message_id: number;
+    message_id: BigNumber;
     snippet_name: string;
     snippet_body: string;
     content_after: string;
 }
 
-export interface CommandEvent extends Event {
+export interface CommandEvent extends MessageEvent {
     event_type: 'command';
     prefix: string;
     command_name: string;
@@ -47,89 +49,28 @@ export interface CommandEvent extends Event {
     targets?: any[];
 }
 
-// class RecordedButtonInteraction(RecordedEvent):
-// """
-// A button was clicked in a recorded channel.
-//     Causality: This is the first event in an interaction context.
-// """
-//
-// event_type = "button_press"
-// interaction_id: int
-// interaction_message_id: int
-// author_id: str
-// author_name: str
-// button_id: str
-// button_label: str
-//
-// @classmethod
-// def from_interaction(cls, combat_id: str, interaction: disnake.MessageInteraction):
-// return cls(
-//     combat_id=combat_id,
-//     interaction_id=interaction.id,
-//     interaction_message_id=interaction.message.id,
-//     author_id=interaction.author.id,
-//     author_name=interaction.author.display_name,
-//     button_id=interaction.data.custom_id,
-//     button_label=interaction.component.label,
-// )
-//
-//
-// class RecordedAutomation(RecordedEvent):
-// """
-// An Automation document finished executing in a recorded channel.
-//     Causality:
-// - Must occur after a RecordedMessage or RecordedButtonInteraction with the same ``message_id`` or ``interaction_id``
-// - If message, must occur before RecordedCommandInvocation with the same ``message_id``
-// - Must occur before a RecordedCombatState which *may* have the same ``interaction_id``
-// """
-//
-// event_type = "automation_run"
-// interaction_id: int
-// automation: Any
-// automation_result: Any
-// caster: Optional[dict]  # this is a StatBlock, typed as dict since StatBlock does not inherit from BaseModel
-// targets: Optional[List[dict]]
-//
-// @classmethod
-// def new(
-//     cls,
-//         ctx: Union["AvraeContext", disnake.Interaction],
-//     combat: "Combat",
-//     automation: "Automation",
-//     automation_result: "AutomationResult",
-//     caster: "StatBlock",
-//     targets: List[Union["StatBlock", str]],
-// ):
-// return cls(
-//     combat_id=combat.nlp_record_session_id,
-//     interaction_id=interaction_id(ctx),
-//     automation=automation.to_dict(),
-//     automation_result=automation_result.to_dict(),
-//     caster=caster.to_dict(),
-//     targets=[t.to_dict() if hasattr(t, "to_dict") else t for t in targets],
-// )
-//
-//
-// class RecordedCombatState(RecordedEvent):
-// """
-// The recorded combat has been committed.
-//     Causality:
-// - Must occur after a RecordedMessage or RecordedButtonInteraction which *may* have the same ``message_id`` or
-//     ``interaction_id``
-// - Must occur before RecordedCommandInvocation which *may* have the same ``message_id`` or ``interaction_id``
-// """
-//
-// event_type = "combat_state_update"
-// # due to caching this might not actually be the interaction this state update is tied to
-// probable_interaction_id: int
-// data: Any
-// human_readable: str
-//
-// @classmethod
-// def from_combat(cls, combat: "Combat", ctx: Union["AvraeContext", disnake.Interaction]):
-// return cls(
-//     combat_id=combat.nlp_record_session_id,
-//     probable_interaction_id=interaction_id(ctx),
-//     data=combat.to_dict(),
-//     human_readable=combat.get_summary(private=True),
-// )
+export interface ButtonPressEvent extends Event {
+    event_type: 'button_press';
+    interaction_id: BigNumber;
+    interaction_message_id: BigNumber;
+    author_id: string;
+    author_name: string;
+    button_id: string;
+    button_label: string;
+}
+
+export interface AutomationRunEvent extends Event {
+    event_type: 'automation_run';
+    interaction_id: BigNumber;
+    automation: any;
+    automation_result: any;
+    caster?: any;
+    targets?: any[];
+}
+
+export interface CombatStateEvent extends Event {
+    event_type: 'combat_state_update';
+    probable_interaction_id: BigNumber;
+    data: any;
+    human_readable: string;
+}
--- explorer/src/events.ts
+++ explorer/src/events.ts
@@ -1,9 +1,25 @@
 /*
 * Update this file for dataset-specific implementations.
 */
+import type {
+    AliasResolutionEvent,
+    AutomationRunEvent,
+    ButtonPressEvent,
+    CombatStateEvent,
+    CommandEvent,
+    MessageEvent,
+    SnippetResolutionEvent
+} from "@/avrae/events";
 
 // to operate on raw JSON, set AnyEvent = any
-export type AnyEvent = any;
+export type AnyEvent =
+    MessageEvent
+    | AliasResolutionEvent
+    | SnippetResolutionEvent
+    | CommandEvent
+    | ButtonPressEvent
+    | AutomationRunEvent
+    | CombatStateEvent;
 
 // change this implementation for your event schema
 export function getEventId(event: AnyEvent): string | null {
