82b03dd0c2831f6f004fb8b01703ce44e9ea1b64
Andrew Zhu <me@andrew-zhu.com>
2022-08-24 09:42:54 -0400

    feat: instance viewer scaffolding

--- explorer/src/views/InstanceViewer.vue
+++ explorer/src/views/InstanceViewer.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import EventComponent from "@/avrae/EventComponent.vue";
 import {DatasetClient} from "@/client";
-import {onMounted, reactive} from "vue";
+import Paginator from "@/components/Paginator.vue";
+import type {AnyEvent} from "@/events";
+import {computed, onMounted, reactive, ref} from "vue";
 
 // component setup
 const props = defineProps<{
@@ -9,26 +11,54 @@ const props = defineProps<{
   instanceId: string;
 }>();
 
+
 // data
-const events = reactive<any[]>([]);
+const events = reactive<AnyEvent[]>([]);
+const isLoading = ref(true);
+const pagination = reactive({
+  currentPage: 0,
+  numPerPage: 250
+});
+
+// computed
+const numPages = computed(() => Math.ceil(events.length / pagination.numPerPage));
+const currentPageEvents = computed(() => events.slice(pagination.currentPage * pagination.numPerPage, (pagination.currentPage + 1) * pagination.numPerPage));
 
 // hooks
 onMounted(async () => {
   for await (const event of props.client.loadEventsForInstance(props.instanceId)) {
     events.push(event);
   }
+  isLoading.value = false;
 });
 </script>
 
 <template>
   <div class="container">
     <section class="section">
-      <p>
-        you are viewing {{ instanceId }}
+      <h1 class="title">Instance Viewer</h1>
+      <p class="subtitle mb-1">Instance <i>{{ instanceId }}</i></p>
+      <p class="content is-small">
+        <RouterLink to="/">Back to Instances</RouterLink>
       </p>
-      <p>
-        {{ events.length }}
-      </p>
+
+      <div class="content">
+        <ul>
+          <li>Events loaded: {{ events.length }} <span v-if="isLoading">(loading...)</span></li>
+          <li v-for="heuristic in client.heuristicIds">
+            {{ heuristic }}: {{ client.heuristicsByInstance[instanceId][heuristic] }}
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="section">
+      <EventComponent v-for="event in currentPageEvents" :event="event"/>
+
+      <Paginator :current-page="pagination.currentPage"
+                 :num-pages="numPages"
+                 @previous-page="pagination.currentPage--"
+                 @next-page="pagination.currentPage++"/>
     </section>
   </div>
 </template>
