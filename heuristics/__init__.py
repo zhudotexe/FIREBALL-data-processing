16db4392ad28dca153105ba4a7956b308441d4b0
Andrew Zhu <me@andrew-zhu.com>
2022-10-06 22:50:31 -0400

    chore: add num_turns, num_words_per_turn

--- heuristics/__init__.py
+++ heuristics/__init__.py
@@ -3,7 +3,13 @@ from typing import Callable, Iterable
 # import heuristic implementations here
 from .count import event_count, message_count
 from .ratio import average_message_length, message_to_command_ratio
-from .zhu import avg_num_words_between_commands, num_participants
+from .zhu import (
+    avg_num_words_between_commands,
+    num_participants,
+    num_turns,
+    num_words_per_turn,
+    words_between_commands_excl_last,
+)
 
 # register heuristic implementations here
 __all__ = (
@@ -13,6 +19,8 @@ __all__ = (
     "average_message_length",
     "avg_num_words_between_commands",
     "num_participants",
+    "num_turns",
+    "num_words_per_turn",
 )
 
 # typing helpers
--- heuristics/zhu.py
+++ heuristics/zhu.py
@@ -19,6 +19,22 @@ def avg_num_words_between_commands(events) -> float:
     return (sum(counts) / len(counts)) if counts else 0
 
 
+def words_between_commands_excl_last(events) -> float:
+    """
+    Same as avg_num_words_between_commands, but excluding the last buffer.
+    """
+    count = 0
+    counts = []
+    for event in events:
+        if event["event_type"] == "command":
+            counts.append(count)
+            count = 0
+        elif event["event_type"] == "message" and not is_bot_message(event) and not is_command_invocation(event):
+            count += len(event["content"].split(" "))
+
+    return (sum(counts) / len(counts)) if counts else 0
+
+
 def num_participants(events) -> float:
     """Returns the number of unique message authors in an event stream."""
     authors = set()
@@ -26,3 +42,25 @@ def num_participants(events) -> float:
         if event["event_type"] == "message":
             authors.add(event["author_id"])
     return len(authors)
+
+
+def num_turns(events) -> float:
+    """Returns the number of combat turns in an event stream."""
+    return sum(1 for event in events if event["event_type"] == "command" and event["command_name"] == "init next")
+
+
+def num_words_per_turn(events) -> float:
+    """
+    Returns the average number of words per combat turn, excluding the last turn (to avoid "forgetting to end"
+    outliers).
+    """
+    count = 0
+    counts = []
+    for event in events:
+        if event["event_type"] == "command" and event["command_name"] == "init next":
+            counts.append(count)
+            count = 0
+        elif event["event_type"] == "message" and not is_bot_message(event) and not is_command_invocation(event):
+            count += len(event["content"].split(" "))
+
+    return (sum(counts) / len(counts)) if counts else 0
