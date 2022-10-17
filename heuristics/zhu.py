fe0455867a5060ee65321163da663fd770f56f40
Andrew Zhu <me@andrew-zhu.com>
2022-10-17 11:18:16 -0400

    chore: add additional heuristics

--- heuristics/zhu.py
+++ heuristics/zhu.py
@@ -1,3 +1,5 @@
+import itertools
+
 from .utils import is_bot_message, is_command_invocation
 
 
@@ -42,6 +44,55 @@ def num_participants(events) -> float:
         if event["event_type"] == "message":
             authors.add(event["author_id"])
     return len(authors)
+
+
+def num_actors(events) -> float:
+    """Returns the number of initiative actors in an event stream."""
+    actor_ids = set()
+    for event in events:
+        if event["event_type"] == "combat_state_update":
+            combat = event["data"]
+            for actor in combat["combatants"]:
+                if actor["type"] == "group":
+                    actor_ids.update({group_actor["id"] for group_actor in actor["combatants"]})
+                else:
+                    actor_ids.add(actor["id"])
+
+    return len(actor_ids)
+
+
+def num_player_actors(events) -> float:
+    """Returns the number of player actors in an event stream."""
+    return sum(1 for event in events if event["event_type"] == "command" and event["command_name"] == "init join")
+
+
+def num_monster_actors(events) -> float:
+    """Returns the number of monster actors in an event stream."""
+    actor_ids = set()
+    for event in events:
+        if event["event_type"] == "combat_state_update":
+            combat = event["data"]
+            for actor in combat["combatants"]:
+                if actor["type"] == "group":
+                    actor_ids.update(
+                        {group_actor["id"] for group_actor in actor["combatants"] if group_actor["type"] == "monster"}
+                    )
+                elif actor["type"] == "monster":
+                    actor_ids.add(actor["id"])
+
+    return len(actor_ids)
+
+
+def player_to_monster_ratio(events) -> float:
+    """
+    Returns the ratio of players to monsters in an event stream.
+    Returns 255 as a sentinel value if there are no monsters.
+    WARNING: eats a lot of memory due to consuming the iterator twice.
+    """
+    e1, e2 = itertools.tee(events)
+    num_player = num_player_actors(e1)
+    num_monster = num_monster_actors(e2)
+    return 255 if not num_monster else (num_player / num_monster)
 
 
 def num_turns(events) -> float:
