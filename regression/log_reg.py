ec5e40d25f459243fb5f3edae5d40d0ae2f8d068
ahfeng <alexfeng99@gmail.com>
2022-11-04 10:57:16 -0400

    updated validation metric calculation

--- regression/log_reg.py
+++ regression/log_reg.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
 from sklearn import preprocessing
 from sklearn.metrics import f1_score, accuracy_score, classification_report
 parser = argparse.ArgumentParser(description="Fits a Logistic Regression model and predicts labels for a dataset")
@@ -29,7 +29,7 @@ parser.add_argument("--class_weight", \
     help="class weight for logisitic regression", default='balanced')
 parser.add_argument("--metric", type=str, \
     help="Evaluation metric", default="f1", choices=["f1","accuracy"])
-parser.add_argument("--splits", help="number of splits to use for cross-validation", default=5)
+parser.add_argument("--splits", help="number of splits to use for cross-validation, -1 for leave-one-out", default=5)
 args = parser.parse_args()
 
 def main(args):
@@ -37,14 +37,20 @@ def main(args):
     data = pd.read_csv(args.train, header=0, index_col=0)
     targets = ["RP to CMD", "CMD to NARR", "RP to CMD or CMD to NARR", "RP to CMD and CMD to NARR"]
     X = data.drop(targets, axis=1)
-    kf = StratifiedKFold(shuffle=True, random_state=23, n_splits=args.splits)
+    splits = len(X) if args.splits == -1 else args.splits
+    kf = StratifiedKFold(shuffle=True, random_state=23, n_splits=splits)
     ys = {target : data[target] for target in targets}
-    best_Cs = {}
-    val_scores = {target : {C : [] for C in args.Cs} for target in targets} # val_accs[target][C] 
+    if args.classifier in ["log_reg", "svm"]:
+        best_Cs = {}
+        val_scores = {target : {C : [] for C in args.Cs} for target in targets} 
+    else: # for classifiers without a C parameter
+        val_scores = {target : [] for target in targets}
     scaler = preprocessing.StandardScaler().fit(X)
     X_scaled = scaler.transform(X)
     metric = f1_score if args.metric == "f1" else accuracy_score
-    # Run K-Fold CV
+    # Run K-Fold CV, tracking total validation predictions and labels
+    y_val_total = {target : [] for target in targets}
+    y_val_pred_total = {target : [] for target in targets}
     for target in targets:
         for train_index, val_index in kf.split(X, ys[target]):
             X_train, X_val = X.iloc[train_index], X.iloc[val_index]
@@ -53,30 +59,48 @@ def main(args):
             X_val_scaled = scaler.transform(X_val)
             y_train = ys[target].iloc[train_index]
             y_val = ys[target].iloc[val_index]
-            for C in args.Cs:
-                if args.classifier == "log_reg":
-                    model = LogisticRegression(C=C, class_weight=args.class_weight, \
-                        penalty=args.penalty).fit(X_train_scaled, y_train)
-                elif args.classifier == "svm":
-                    model = SVC(C=C, class_weight=args.class_weight, \
-                        kernel='linear').fit(X_train_scaled, y_train)
-                elif args.classifier == "naive_bayes":
-                    model = GaussianNB().fit(X_train_scaled, y_train)
-                val_scores[target][C].append(metric(y_val, model.predict(X_val_scaled)))
+            if args.classifier in ["log_reg", "svm"]:
+                for C in args.Cs:
+                    if args.classifier == "log_reg":
+                        model = LogisticRegression(C=C, class_weight=args.class_weight, \
+                            penalty=args.penalty).fit(X_train_scaled, y_train)
+                    elif args.classifier == "svm":
+                        model = SVC(C=C, class_weight=args.class_weight, \
+                            kernel='linear').fit(X_train_scaled, y_train)
+                    y_pred = model.predict(X_val_scaled)
+                    y_val_total[target] += y_val.tolist()
+                    y_val_pred_total[target] += y_pred.toList()
+                    val_scores[target][C].append(metric(y_val, y_pred))
+            elif args.classifier == "naive_bayes":
+                model = GaussianNB().fit(X_train_scaled, y_train)
+                y_pred = model.predict(X_val_scaled)
+                y_val_total[target] += y_val.tolist()
+                y_val_pred_total[target] += y_pred.tolist()
+                val_scores[target].append(metric(y_val, y_pred))
+
+                
 
     # Find best C for each target by mean validation accuracy
+    y_val_total = {target : pd.DataFrame(y_val_total[target]).fillna(-1) for target in targets}
+    y_val_pred_total = {target : pd.DataFrame(y_val_pred_total[target]).fillna(-1) for target in targets}
+    for target in targets:
+        print(len(y_val_total[target]))
+        print(len(y_val_pred_total[target]))
     with open(args.classifier+"_"+args.val_out, 'w') as f:
         for target in targets:
-            best_val_score = 0
-            for C in args.Cs:
-                t_val_score = np.mean(val_scores[target][C])
-                if t_val_score > best_val_score:
-                    best_C = C
-                    best_val_score = t_val_score
-                    best_val_report = classification_report(y_val, model.predict(X_val_scaled))
-            best_Cs[target] = best_C
-            f.write("Best C for {} is {} with mean {} score of {}\n".format(target, best_C, args.metric, best_val_score))
+            best_val_report = classification_report(y_val_total[target], y_val_pred_total[target])
+            f.write("Validation Report for "+target+":\n")
             f.write(best_val_report)
+            best_val_score = 0
+            if args.classifier in ["log_reg", "svm"]:
+                for C in args.Cs:
+                    t_val_score = np.mean(val_scores[target]) if args.classifier == 'naive_bayes' else np.mean(val_scores[target][C])
+                    if t_val_score > best_val_score:
+                        best_C = C
+                        best_val_score = t_val_score
+                        best_Cs[target] = best_C
+                        # f.write("Best C for {} is {} with mean {} score of {}\n".format(target, best_C, args.metric, best_val_score))
+           
           
     # Run Logistic Regression over the whole dataset with selected C values
     full = pd.read_csv(args.unlabeled, header=0, index_col=0)
@@ -106,8 +130,8 @@ if __name__ == "__main__":
     main(args)
     # log validation metrics, add F1, metrics for each class DONE
     # try class weights DONE, improved F1
-    # stratified k-fold 
-    # output list of instance ids
+    # stratified k-fold DONE
+    # output list of instance ids DONE
     # Automate train data generation
-    # Try SVM, NaiveBayes
+    # Try SVM, NaiveBayes DONE
     # PCA?
