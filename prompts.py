ef59ce28d6c3d3025cbc36802f593e7973e8c495
ahfeng <alexfeng99@gmail.com>
2022-11-11 13:12:41 -0500

    actor ablations

--- prompts.py
+++ prompts.py
@@ -3,20 +3,20 @@ COMMAND_SEP = "\n<|csep|>\n"
 STOP_SEQ = "\n<|aeot|>"
 
 
-def stringify_actor(actor: dict):
+def stringify_actor(actor: dict, ablations = []):
     # Name (Race/creature type; class if available) <X/Y HP> [Effects]
     short_parts = [actor["name"]]
     race_and_class_parts = []
-    if actor["race"]:
+    if "race" not in ablations and actor["race"]:
         race_and_class_parts.append(actor["race"])
-    if actor["class"]:
+    if "class" not in ablations and actor["class"]:
         race_and_class_parts.append(actor["class"])
     race_and_class = "; ".join(race_and_class_parts)
 
     if race_and_class:
         short_parts.append(f"({race_and_class})")
     short_parts.append(actor["hp"])
-    if actor["effects"]:
+    if "effects" not in ablations and actor["effects"]:
         short_parts.append(f"[{actor['effects']}]")
 
     # Description: ...
@@ -34,24 +34,29 @@ def stringify_actor(actor: dict):
     # Actions:
     # Effects:
     long_parts = [f"Name: {actor['name']}"]
-    if actor["class"]:
-        long_parts.append(f"Class: {actor['class']}")
-    if actor["race"]:
-        long_parts.append(f"Race: {actor['race']}")
-    if actor["attacks"]:
-        long_parts.append(f"Attacks: {actor['attacks']}")
-    if actor["spells"]:
-        long_parts.append(f"Spells: {actor['spells']}")
-    if actor["actions"]:
-        long_parts.append(f"Actions: {actor['actions']}")
-    if actor["effects"]:
-        long_parts.append(f"Effects: {actor['effects']}")
+    for part in ["Name", "Class", "Race", "Attacks", "Spells", "Actions", "Effects"]:
+        if part.lower() in ablations:
+            continue
+        if actor[part.lower()]:
+            long_parts.append(f"{part}: {actor[part.lower()]}")
+    # if "class" not in ablations and actor["class"]:
+    #     long_parts.append(f"Class: {actor['class']}")
+    # if "race" not in ablations and actor["race"]:
+    #     long_parts.append(f"Race: {actor['race']}")
+    # if "attacks" not in ablations and actor["attacks"]:
+    #     long_parts.append(f"Attacks: {actor['attacks']}")
+    # if actor["spells"]:
+    #     long_parts.append(f"Spells: {actor['spells']}")
+    # if actor["actions"]:
+    #     long_parts.append(f"Actions: {actor['actions']}")
+    # if actor["effects"]:
+    #     long_parts.append(f"Effects: {actor['effects']}")
 
     return {"short": " ".join(short_parts), "long": "\n".join(long_parts), "description": description}
 
 # parts for ablation: actors, current actor, and their constituent parts
 # possible ablations = ["actors","current"]
-def utt_cmd_prompt(data, include_sep=True, ablations = []) -> str | None:
+def utt_cmd_prompt(data, include_sep=True, ablations = [], actor_ablations=[]) -> str | None:
     before = data["before_utterances"]
     state_before = data["combat_state_before"]
     current = data["current_actor"]
@@ -81,13 +86,13 @@ def utt_cmd_prompt(data, include_sep=True, ablations = []) -> str | None:
     # command
     # <|aeot|>
     prompt_parts = []
-    actors = [f"- {stringify_actor(a)['short']}" for a in state_before]
+    actors = [f"- {stringify_actor(a, ablations=actor_ablations)['short']}" for a in state_before]
     actors_prompt = f"Actors:\n" + "\n".join(actors)
     if actors and "actors" not in ablations:
         prompt_parts.append(actors_prompt)
     if "current" not in ablations:
         if current is not None:
-            prompt_parts.append(f"Current:\n{stringify_actor(current)['long']}")
+            prompt_parts.append(f"Current:\n{stringify_actor(current, ablations=actor_ablations)['long']}")
         else:
             prompt_parts.append("Current:\nNone")
 
@@ -104,7 +109,7 @@ def utt_cmd_completion(data, include_sep=True, command_sep=COMMAND_SEP) -> str |
     return command_sep.join(commands) + (STOP_SEQ if include_sep else "")
 
 # ablations: actors, targets, caster
-def sta_nar_prompt(data, include_sep=True, ablations = []) -> str | None:
+def sta_nar_prompt(data, include_sep=True, ablations = [], actor_ablations=[]) -> str | None:
     state_after = data["combat_state_after"]
     caster = data["caster_after"]
     targets = data["targets_after"]
@@ -139,19 +144,19 @@ def sta_nar_prompt(data, include_sep=True, ablations = []) -> str | None:
 
     prompt_parts = []
     if "actors" not in ablations:
-        actors = [f"- {stringify_actor(a)['short']}" for a in state_after]
+        actors = [f"- {stringify_actor(a, ablations=actor_ablations)['short']}" for a in state_after]
         actors_prompt = f"Actors:\n" + "\n".join(actors)
         if actors:
             prompt_parts.append(actors_prompt)
 
     if "targets" not in ablations:
-        targets_str = [f"- {stringify_actor(a)['short']}" for a in targets]
+        targets_str = [f"- {stringify_actor(a, ablations=actor_ablations)['short']}" for a in targets]
         targets_prompt = f"Targets:\n" + "\n".join(targets_str)
         if targets:
             prompt_parts.append(targets_prompt)
 
     if "caster" not in ablations:
-        caster_strs = stringify_actor(caster)
+        caster_strs = stringify_actor(caster, ablations=actor_ablations)
         prompt_parts.append(f"{caster_strs['description']}{caster_strs['long']}")
 
     prompt_parts.append("\n".join(automation_results))
