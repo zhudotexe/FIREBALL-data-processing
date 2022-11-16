7e6eba4b7ce2065c34c18437d594eded82157f00
Andrew Zhu <me@andrew-zhu.com>
2022-11-15 22:02:07 -0500

    distill4: extract automation embed event

--- distill4_normalize.py
+++ distill4_normalize.py
@@ -33,7 +33,7 @@ import tqdm.contrib.concurrent
 import tqdm.contrib.logging
 
 from dataset.utils import combat_dir_iterator, read_gzipped_file, write_jsonl
-from heuristics.utils import Event, Instance, MessageGroup
+from heuristics.utils import AVRAE_ID, Event, Instance, MessageGroup
 
 # hack to add avrae submodule to pypath
 # if this errors, pip install -r avrae/requirements.txt
@@ -188,7 +188,12 @@ class Distill4Inst(Instance):
             "controller_id": str(combatant.controller_id),  # TODO make this not use discord ID
         }
 
-    def stringify_automation_run(self, event):
+    def stringify_automation_run(self, event: Event) -> tuple[str, Event]:
+        """
+        Given an automation run event, returns a string representation of that event and the corresponding Message event
+        from Avrae with the result embed.
+        """
+        # stringification
         caster = event["caster"]["name"]
         targets = [(t["name"] if not isinstance(t, str) else t) for t in event["targets"]]
 
@@ -264,7 +269,20 @@ class Distill4Inst(Instance):
                     )
                     return f"{base}\n{children}"
 
-        return stringify(event["automation_result"])
+        # embed finding
+        message_group = self.message_groups_by_id[event["interaction_id"]]
+        embed_event = self.find(
+            lambda e: e["event_type"] == "message"
+            and e["author_id"] == AVRAE_ID
+            and e["content"] == ""
+            and len(e["embeds"]) == 1,
+            after=message_group.events[0],
+            before=message_group.events[-1],
+        )
+        if embed_event is None:
+            log.warning(f"Could not find embed for automation run")
+
+        return stringify(event["automation_result"]), embed_event
 
     # ==== normalizers =====
     def normalize_message(self, msg: Event) -> str:
@@ -382,8 +400,11 @@ class Distill4Inst(Instance):
 
         # stringify automation run
         automation_norm = []
+        embed_idxs = []
         for e in commands_inst.find_all_of_type("automation_run"):
-            automation_norm.append(self.stringify_automation_run(e))
+            run_result_str, embed_event = self.stringify_automation_run(e)
+            automation_norm.append(run_result_str)
+            embed_idxs.append(self.events.index(embed_event))
 
         # state after
         self.extract_characters_backward(commands[-1])
@@ -418,6 +439,8 @@ class Distill4Inst(Instance):
             "command_idxs": [self.events.index(b) for b in commands],
             "after_state_idx": after_state_idx,
             "after_idxs": [self.events.index(b) for b in after],
+            # other useful message idxs
+            "embed_idxs": embed_idxs,
         }
 
 
