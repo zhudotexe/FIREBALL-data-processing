bd2b6bcc84f3b2d2c1634d9303bf283e81003f79
Andrew Zhu <me@andrew-zhu.com>
2022-11-18 16:27:05 -0500

    Update test.py

--- unit_tests/bardic_inspiration/test.py
+++ unit_tests/bardic_inspiration/test.py
@@ -2,7 +2,7 @@
 from avrae.tests.utils import active_combat
 
 
-async def test_hp_modifications(self, avrae, dhttp):
+async def test_assertion(avrae, dhttp):
     # TODO: load the characters and relevant combat state from characters.json and combat.json
 
     prompt = None  # TODO: load from prompt.txt
@@ -10,7 +10,8 @@ async def test_hp_modifications(self, avrae, dhttp):
     def predict(p):
         return None  # TODO: call GPT-3 (or baseline model)
 
-    avrae.message(predict(prompt))
+    combat = await active_combat(avrae)
+    avrae.message(predict(prompt), author_id=combat.current_combatant.controller_id)
     await dhttp.drain()
 
     # assertions here
