5cd8974918a8cd009e79932a60cb4da828591c5e
Andrew Zhu <me@andrew-zhu.com>
2022-12-12 20:31:51 -0500

    update raw counter script

--- scripts/count_num_chars_in_dataset.py
+++ scripts/count_num_chars_in_dataset.py
@@ -5,12 +5,13 @@ This is useful for estimating inference costs with GPT-3.
 import os.path
 import pathlib
 import sys
+from collections import namedtuple, Counter
 
 import tqdm.contrib.concurrent
 
 sys.path.append("..")
 
-import dataset.utils
+from dataset import utils
 
 DATA_DIR = pathlib.Path(os.path.dirname(__file__), "../data")
 MODEL_COSTS = (
@@ -24,22 +25,57 @@ MODEL_COSTS = (
     ("FT Ada", 0.0016),
 )
 
+Count = namedtuple("Count", "n_chars n_events events commands authors")
 
-def count_tokens_in_dir(dname):
-    return sum(len(event["content"]) for event in utils.combat_dir_iterator(dname) if event["event_type"] == "message")
+
+def count(dname):
+    events = Counter()  # event type -> occurrences
+    commands = Counter()  # command name -> occurrences
+    authors = Counter()  # author id -> number of characters
+    n_chars = 0
+    n_events = 0
+
+    for event in utils.combat_dir_iterator(dname):
+        n_events += 1
+        events[event["event_type"]] += 1
+        if event["event_type"] == "message":
+            msg_len = len(event["content"])
+            n_chars += msg_len
+            authors[event["author_id"]] += msg_len
+        elif event["event_type"] == "command":
+            commands[event["command_name"]] += 1
+
+    return Count(n_chars=n_chars, n_events=n_events, events=events, commands=commands, authors=authors)
 
 
 def main():
-    counts = tqdm.contrib.concurrent.process_map(count_tokens_in_dir, utils.get_combat_dirs(DATA_DIR), chunksize=10)
+    counts = tqdm.contrib.concurrent.process_map(count, utils.get_combat_dirs(DATA_DIR), chunksize=10)
 
-    count = sum(counts)
-    token_count = count / 4
+    total_events = Counter()
+    total_commands = Counter()
+    total_authors = Counter()
+    for c in counts:
+        total_events.update(c.events)
+        total_commands.update(c.commands)
+        total_authors.update(c.authors)
+    total_chars = sum(c.n_chars for c in counts)
+    total_n_events = sum(c.n_events for c in counts)
 
-    print(f"Total number of characters: {count} (approx. {token_count} tokens)")
+    print(
+        f"total number of characters: {total_chars}\n"
+        f"total number of events: {total_n_events}\n"
+        f"total events: {total_events}\n"
+        f"total commands: {total_commands}\n"
+        f"total authors: {total_authors}\n"
+        f"unique authors: {len(total_authors)}"
+    )
+
+    token_count = total_chars / 4
+
+    print(f"Total number of characters: {total_chars} (approx. {token_count} tokens)")
     for model_name, cost_per_1k in MODEL_COSTS:
         print(f"{model_name}: ${token_count / 1000 * cost_per_1k:.2f}")
 
 
 if __name__ == "__main__":
     main()
-
